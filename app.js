0fd77da4eb83838a6d41d15ba295aeb49d0c101c
Axel <dlaxel@hotmail.com>
2018-07-05 00:05:43 +1000

    updated game logic to include relative gameboard swapping

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 console.log('XOXOXOXOX')
 
 // Declaring variables
-
 var allGameBoards = document.querySelectorAll('.game-board')
 var allGameSquares = document.querySelectorAll('.game-square')
 var metaGameBoard = document.querySelector('.meta-game-board')
-var winningCombinations = [[1, 2, 3], [4, 5,6], [7, 8, 9], [1, 4, 7], [2, 5, 8],[3, 6, 9], [1, 4, 9], [3, 4, 6]]
+var winningCombinations = [[1, 2, 3], [4, 5,6], [7, 8, 9], [1, 4, 7], [2, 5, 8],[3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 var bluePlayerNestArr = []
 var redPlayerNestArr = []
+
+var bluePlayerMetaArr = []
+var redPlayerMetaArr = []
 
 allGameBoards.forEach(function () {
   bluePlayerNestArr.push([])
@@ -25,11 +27,10 @@ function placeToken(event) {
   if (event.target.classList.contains('game-square') === false) {
     console.log('not a game square')
     return
+  } else if (event.target.classList.contains('red') === true && event.target.classList.contains('blue') === true)  {
+       return
   }
-
-  if (event.target.classList.contains('red') === true && event.target.classList.contains('blue') === true){
-  return
-  }
+  
 
   var gameBoardIdentity = event.target.parentNode
   var gameBoardIndex = (Number(gameBoardIdentity.dataset.board) - 1)
@@ -37,13 +38,15 @@ function placeToken(event) {
 
   if (playersTurn === 'red-player'){
     event.target.classList.add('red-player')
-    redPlayerNestArr[gameBoardIndex].push(Number(event.target.dataset.cell))
-    checkWin(gameBoardIndex, 'red-player', redPlayerNestArr)
+    redPlayerNestArr[gameBoardIndex].push(gameBoardSelect)
+    checkWin(gameBoardIndex, 'red-player', redPlayerNestArr, redPlayerMetaArr)
+    relativeBoardSelect(gameBoardIndex, gameBoardSelect)
     playersTurn = 'blue-player'
   } else if (playersTurn === 'blue-player'){
     event.target.classList.add('blue-player')
-    bluePlayerNestArr[gameBoardIndex].push(Number(event.target.dataset.cell))
-    checkWin(gameBoardIndex, 'blue-player', bluePlayerNestArr)
+    bluePlayerNestArr[gameBoardIndex].push(gameBoardSelect)
+    checkWin(gameBoardIndex, 'blue-player', bluePlayerNestArr, bluePlayerMetaArr)
+    relativeBoardSelect(gameBoardIndex, gameBoardSelect)
     playersTurn = 'red-player'
   }
 }
@@ -51,11 +54,14 @@ function placeToken(event) {
 // 2 Create a funciton to check for wins - add visual display
 
 // Each if statement checks the 8 possible win variations on the particular game board
-function checkWin(gameBoardIndex, playerName, playerNestArr) {
+function checkWin(gameBoardIndex, playerName, playerNestArr, playerMetaArr) {
   for (let index = 0; index < playerNestArr[gameBoardIndex].length; index++) {
     for (let winningIndex = 0; winningIndex < winningCombinations.length; winningIndex++) {    
       if (playerNestArr[gameBoardIndex].includes(winningCombinations[winningIndex][0]) && playerNestArr[gameBoardIndex].includes(winningCombinations[winningIndex][1]) && playerNestArr[gameBoardIndex].includes(winningCombinations[winningIndex][2])) {
-        console.log(playerName + ' Victory')
+        console.log(playerName + ' Victory' + gameBoardIndex)
+        bluePlayerNestArr[gameBoardIndex].push('finished')
+        redPlayerNestArr[gameBoardIndex].push('finished')
+        playerMetaArr.push(gameBoardIndex + 1)
         return
       }
     }
@@ -89,16 +95,26 @@ function initiateAllListeners() {
 // 5 Create a score board
 
 // 6 Relative board selector
-function boardFocus(gameBoardSelect) {
+function boardFocus(gameBoardIndex, gameBoardSelect) {
   for (let index = 0; index < allGameBoards.length; index++) {
     allGameBoards[index].removeEventListener('click', placeToken)
   }
   allGameBoards[gameBoardSelect - 1].addEventListener('click', placeToken)
 }
-
-function relativeBoardSelect(gameBoardSelect) {
-  boardFocus(gameBoardSelect)
-  // if a square is clicked transfer to relative board
+function focusUnfinishedBoards() {
+  for (let index = 0; index < allGameBoards.length; index++) {
+    if (allGameBoards[index].includes('finished')){
+      allGameBoards[index].removeEventListener('click', placeToken)
+    } else {
+      allGameBoards[index].addEventListener('click', placeToken)
+    }
+  }
 }
-
+function relativeBoardSelect(gameBoardIndex, gameBoardSelect) {
+  if (bluePlayerNestArr[gameBoardIndex].includes('finished') === true){
+    focusUnfinishedBoards()
+  } else {
+    boardFocus(gameBoardIndex, gameBoardSelect)
+  }
+}
 initiateAllListeners()
