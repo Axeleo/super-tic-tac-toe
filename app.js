8f0b01c1aaea2750fb5e3365054fe4cc611c890b
Axel <dlaxel@hotmail.com>
2018-07-04 11:40:35 +1000

    created 9 games boards, which are all functioning individually

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ console.log('XOXOXOXOX')
 var cols1 = document.querySelectorAll('.column1')
 var cols2 = document.querySelectorAll('.column2')
 var cols3 = document.querySelectorAll('.column3')
-var row1 = document.querySelector('.row1')
-var row2 = document.querySelector('.row2')
-var row3 = document.querySelector('.row3')
-var gameBoard = document.querySelector('.game-board')
+
+var row1 = document.querySelectorAll('.row1')
+var row2 = document.querySelectorAll('.row2')
+var row3 = document.querySelectorAll('.row3')
+
+var allGameBoards = document.querySelectorAll('.game-board')
 var gameSquares = document.querySelectorAll('.game-square')
+
+var gameBoardsFirstRow = document.querySelector('.game-boards-first-row')
+var gameBoardsSecondRow = document.querySelector('.game-boards-second-row')
+var gameBoardsThirdRow = document.querySelector('.game-boards-third-row')
+
+var gameBoardsFirstCol = document.querySelector('.game-boards-first-col')
+var gameBoardsSecondCol = document.querySelector('.game-boards-second-col')
+var gameBoardsThirdCol = document.querySelector('.game-boards-third-col')
+
+var metaGameBoard = document.querySelector('.meta-game-board')
+
 var playersTurn = 'red-player'
 
 // ------ Game logic
@@ -25,70 +38,78 @@ function placeToken(event) {
   if (event.target.classList.contains('red') === true && event.target.classList.contains('blue') === true){
   return
   }
+  // set game board identity
+  gameBoardIdentity = event.target.parentNode.parentNode
   // checks which players turn it is and places token
   if (playersTurn === 'red-player'){
     event.target.classList.add('red-player')
     playersTurn = 'blue-player'
-    checkWin('red-player')
+    checkWin(gameBoardIdentity, 'red-player')
   } else if (playersTurn === 'blue-player'){
     event.target.classList.add('blue-player')
     playersTurn = 'red-player'
-    checkWin('blue-player')
+    // !!! add gameboard identity
+    checkWin(gameBoardIdentity, 'blue-player')
   }
 
 }
 
 
 // 2 Create a funciton to check for wins - add visual display
-function checkWin(playerStr) {
+
+// Each if statement checks the 8 possible win variations on the particular game board
+function checkWin(gameBoardIdentity, playerStr) {
   //checking rows
-  if (row1.children[0].classList.contains(playerStr) && row1.children[1].classList.contains(playerStr) && row1.children[2].classList.contains(playerStr)){
+  if (gameBoardIdentity.children[0].children[0].classList.contains(playerStr) && gameBoardIdentity.children[0].children[1].classList.contains(playerStr) && gameBoardIdentity.children[0].children[2].classList.contains(playerStr)) {
     console.log(playerStr + ' Victory')
   }
-  if (row2.children[0].classList.contains(playerStr) && row2.children[1].classList.contains(playerStr) && row2.children[2].classList.contains(playerStr)){
+  if (gameBoardIdentity.children[1].children[0].classList.contains(playerStr) && gameBoardIdentity.children[1].children[1].classList.contains(playerStr) && gameBoardIdentity.children[1].children[2].classList.contains(playerStr)) {
     console.log(playerStr + ' Victory')
   }
-  if (row3.children[0].classList.contains(playerStr) && row3.children[1].classList.contains(playerStr) && row3.children[2].classList.contains(playerStr)){
+  if (gameBoardIdentity.children[2].children[0].classList.contains(playerStr) && gameBoardIdentity.children[2].children[1].classList.contains(playerStr) && gameBoardIdentity.children[2].children[2].classList.contains(playerStr)) {
     console.log(playerStr + ' Victory')
   }
   //checking cols
-  if (row1.children[0].classList.contains(playerStr) && row2.children[0].classList.contains(playerStr) && row3.children[0].classList.contains(playerStr)) {
+  if (gameBoardIdentity.children[0].children[0].classList.contains(playerStr) && gameBoardIdentity.children[1].children[0].classList.contains(playerStr) && gameBoardIdentity.children[2].children[0].classList.contains(playerStr)) {
     console.log(playerStr + ' Victory')
   }
-  if (row1.children[1].classList.contains(playerStr) && row2.children[1].classList.contains(playerStr) && row3.children[1].classList.contains(playerStr)) {
+  if (gameBoardIdentity.children[0].children[1].classList.contains(playerStr) && gameBoardIdentity.children[1].children[1].classList.contains(playerStr) && gameBoardIdentity.children[2].children[1].classList.contains(playerStr)) {
     console.log(playerStr + ' Victory')
   }
-  if (row1.children[2].classList.contains(playerStr) && row2.children[2].classList.contains(playerStr) && row3.children[2].classList.contains(playerStr)) {
+  if (gameBoardIdentity.children[0].children[2].classList.contains(playerStr) && gameBoardIdentity.children[1].children[2].classList.contains(playerStr) && gameBoardIdentity.children[2].children[2].classList.contains(playerStr)) {
     console.log(playerStr + ' Victory')
   }
   // checking diagonals
-  if (row1.children[0].classList.contains(playerStr) && row2.children[1].classList.contains(playerStr) && row3.children[2].classList.contains(playerStr)) {
+  if (gameBoardIdentity.children[0].children[0].classList.contains(playerStr) && gameBoardIdentity.children[1].children[1].classList.contains(playerStr) && gameBoardIdentity.children[2].children[2].classList.contains(playerStr)) {
     console.log(playerStr + ' Victory')
   }
-  if (row1.children[2].classList.contains(playerStr) && row2.children[1].classList.contains(playerStr) && row3.children[0].classList.contains(playerStr)) {
+  if (gameBoardIdentity.children[0].children[2].classList.contains(playerStr) && gameBoardIdentity.children[1].children[1].classList.contains(playerStr) && gameBoardIdentity.children[2].children[0].classList.contains(playerStr)) {
     console.log(playerStr + ' Victory')
   }
 }
-// 3 Create event listeners
-gameBoard.addEventListener('click', placeToken)
 
-// 4 Create a reset feature
+// 3 Create a reset feature
 function removePlayerClass(object) {
   object.classList.remove('blue-player')
   object.classList.remove('red-player')
 }
 function resetBoard() {
-  var row1 = document.querySelector('.row1')
-  var row2 = document.querySelector('.row2')
-  var row3 = document.querySelector('.row3')
+  // need to make a new search so that the classes update
+  var row1 = document.querySelectorAll('.row1')
+  var row2 = document.querySelectorAll('.row2')
+  var row3 = document.querySelectorAll('.row3')
+  // loop through each row and call removePlayerClass
   for (var i = 0; i < 3; i++){
     removePlayerClass(row1.children[i])
     removePlayerClass(row2.children[i])
     removePlayerClass(row3.children[i])
   }
- 
 }
+// 4 Create event listeners
+document.body.addEventListener('click', placeToken)
+
 
 
 
 // 5 Create a score board
+
