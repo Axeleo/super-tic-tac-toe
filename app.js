8a15bdf64d63c43a2f776e15ad31be8d828ccb96
Axel <dlaxel@hotmail.com>
2018-07-05 21:21:30 +1000

    fixed hover classes so they reset everytime the baord is changed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var allGameSquares = document.querySelectorAll('.game-square')
 var metaGameBoard = document.querySelector('.meta-game-board')
 var bluePlayerWinDisplay = document.querySelector('.blue-win-count')
 var yellowPlayerWinDisplay = document.querySelector('.yellow-win-count')
-var winningCombinations = [[1, 2, 3], [4, 5,6], [7, 8, 9], [1, 4, 7], [2, 5, 8],[3, 6, 9], [1, 5, 9], [3, 5, 7]]
+var winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 var bluePlayerNestArr = []
 var yellowPlayerNestArr = []
@@ -25,24 +25,24 @@ function placeToken(event) {
   if (event.target.classList.contains('game-square') === false) {
     console.log('not a game square')
     return
-  } else if (event.target.classList.contains('yellow-player') === true || 
-  event.target.classList.contains('blue-player') === true)  {
-      return
+  } else if (event.target.classList.contains('yellow-player') === true ||
+    event.target.classList.contains('blue-player') === true) {
+    return
   }
-  
+
 
   var gameBoardIdentity = event.target.parentNode
   var gameBoardIndex = (Number(gameBoardIdentity.dataset.board) - 1)
   var gameBoardSelect = Number(event.target.dataset.cell)
 
-  if (playersTurn === 'yellow-player'){
+  if (playersTurn === 'yellow-player') {
     event.target.classList.add('yellow-player')
     yellowPlayerNestArr[gameBoardIndex].push(gameBoardSelect)
     checkWin(gameBoardIndex, 'yellow-player', yellowPlayerNestArr, yellowPlayerMetaArr)
     relativeBoardSelect(gameBoardSelect)
     playersTurn = 'blue-player'
     console.log('class should be yellow')
-  } else if (playersTurn === 'blue-player'){
+  } else if (playersTurn === 'blue-player') {
     event.target.classList.add('blue-player')
     bluePlayerNestArr[gameBoardIndex].push(gameBoardSelect)
     checkWin(gameBoardIndex, 'blue-player', bluePlayerNestArr, bluePlayerMetaArr)
@@ -57,10 +57,10 @@ function placeToken(event) {
 // Each if statement checks the 8 possible win variations on the particular game board
 function checkWin(gameBoardIndex, playerName, playerNestArr, playerMetaArr) {
   for (let index = 0; index < playerNestArr[gameBoardIndex].length; index++) {
-    for (let winningIndex = 0; winningIndex < winningCombinations.length; winningIndex++) {    
-      if (playerNestArr[gameBoardIndex].includes(winningCombinations[winningIndex][0]) 
-      && playerNestArr[gameBoardIndex].includes(winningCombinations[winningIndex][1]) 
-      && playerNestArr[gameBoardIndex].includes(winningCombinations[winningIndex][2])) {
+    for (let winningIndex = 0; winningIndex < winningCombinations.length; winningIndex++) {
+      if (playerNestArr[gameBoardIndex].includes(winningCombinations[winningIndex][0])
+        && playerNestArr[gameBoardIndex].includes(winningCombinations[winningIndex][1])
+        && playerNestArr[gameBoardIndex].includes(winningCombinations[winningIndex][2])) {
         console.log(playerName + ' Victory on board ' + (gameBoardIndex + 1))
         bluePlayerNestArr[gameBoardIndex].push('finished')
         yellowPlayerNestArr[gameBoardIndex].push('finished')
@@ -73,15 +73,15 @@ function checkWin(gameBoardIndex, playerName, playerNestArr, playerMetaArr) {
 }
 function checkMetaWin(playerMetaArr, playerName) {
   for (let winningIndex = 0; winningIndex < winningCombinations.length; winningIndex++) {
-    if (playerMetaArr.includes(winningCombinations[winningIndex][0]) 
-    && playerMetaArr.includes(winningCombinations[winningIndex][1]) 
-    && playerMetaArr.includes(winningCombinations[winningIndex][2])) {
+    if (playerMetaArr.includes(winningCombinations[winningIndex][0])
+      && playerMetaArr.includes(winningCombinations[winningIndex][1])
+      && playerMetaArr.includes(winningCombinations[winningIndex][2])) {
       console.log(playerName + ' Grand Victory')
-      if (playerName === 'blue-player'){
+      if (playerName === 'blue-player') {
         bluePlayerWinCount += 1
         bluePlayerWinDisplay.textContent = bluePlayerWinCount
       } else {
-        yellowPlayerWinCount +=1
+        yellowPlayerWinCount += 1
         yellowPlayerWinDisplay.textContent = yellowPlayerWinCount
       }
     }
@@ -93,10 +93,16 @@ function removePlayerClass(object) {
   object.classList.remove('yellow-player')
   object.classList.remove('unfocus')
 }
-function removeHoverAnimationEffect(square) {
-  square.classList.remove('shadow-pop-tr-blue')
-  square.classList.remove('shadow-pop-tr-yellow')
-  square.removeEventListener('mouseover', hoverAnimation)
+// function removeHoverAnimationEffect() {
+//   square.classList.remove('shadow-pop-tr-blue')
+//   square.classList.remove('shadow-pop-tr-yellow')
+//   square.removeEventListener('mouseover', hoverAnimation)
+// }
+function removeAllHoverAnimationClass() {
+  for (let index = 0; index < allGameSquares.length; index++) {
+    allGameSquares[index].classList.remove('shadow-pop-tr-blue')
+    allGameSquares[index].classList.remove('shadow-pop-tr-yellow')
+  }
 }
 function resetGameArray() {
   bluePlayerNestArr = []
@@ -107,8 +113,8 @@ function resetGameArray() {
   })
 }
 function resetBoard() {
-allGameSquares.forEach(removePlayerClass)
-resetGameArray()
+  allGameSquares.forEach(removePlayerClass)
+  resetGameArray()
   for (let index = 0; index < allGameSquares.length; index++) {
     allGameSquares[index].addEventListener('mouseover', hoverAnimation)
   }
@@ -124,37 +130,39 @@ function boardFocus(gameBoardSelect) {
   for (let index = 0; index < allGameBoards.length; index++) {
     allGameBoards[index].removeEventListener('click', placeToken)
     allGameBoards[index].classList.add('unfocus')
-    for (let secondIndex = 0; secondIndex < allGameBoards[index].length; secondIndex++) {
-      removeHoverAnimationEffect(allGameBoards[index].children[secondIndex])
-      console.log(secondIndex)
-    }
+    removeAllHoverAnimationClass()
+    // for (let secondIndex = 0; secondIndex < allGameBoards[index].length; secondIndex++) {
+    //   removeHoverAnimationEffect(allGameBoards[index].children[secondIndex])
+    //   console.log(secondIndex)
+    // }
   }
   allGameBoards[gameBoardSelect - 1].addEventListener('click', placeToken)
   allGameBoards[gameBoardSelect - 1].classList.remove('unfocus')
-  for (let thirdIndex = 0; thirdIndex < allGameBoards[gameBoardSelect - 1].length; thirdIndex++) {
-    allGameBoards[index].children[thirdIndex].addEventListener('mouseover', hoverAnimation)
-  }
+  // for (let thirdIndex = 0; thirdIndex < allGameBoards[gameBoardSelect - 1].length; thirdIndex++) {
+  //   allGameBoards[index].children[thirdIndex].addEventListener('mouseover', hoverAnimation)
+  // }
 }
 function focusUnfinishedBoards() {
   for (let index = 0; index < allGameBoards.length; index++) {
-    if (bluePlayerNestArr[index].includes('finished')){
+    if (bluePlayerNestArr[index].includes('finished')) {
       allGameBoards[index].removeEventListener('click', placeToken)
       allGameBoards[index].classList.add('unfocus')
-      for (let secondIndex = 0; secondIndex < allGameBoards[index].length; secondIndex++) {
-        removeHoverAnimationEffect(allGameBoards[index].children[secondIndex])
-        console.log(allGameBoards[index].children[secondIndex])
-      }
+      removeAllHoverAnimationClass()
+      // for (let secondIndex = 0; secondIndex < allGameBoards[index].length; secondIndex++) {
+      //   removeHoverAnimationEffect(allGameBoards[index].children[secondIndex])
+      //   console.log(allGameBoards[index].children[secondIndex])
+      // }
     } else {
       allGameBoards[index].addEventListener('click', placeToken)
       allGameBoards[index].classList.remove('unfocus')
-      for (let index = 0; index < allGameBoards[index].length; index++) {
-        allGameSquares[index].addEventListener('mouseover', hoverAnimation)
-      }
+      // for (let index = 0; index < allGameBoards[index].length; index++) {
+      //   allGameSquares[index].addEventListener('mouseover', hoverAnimation)
+      // }
     }
   }
 }
 function relativeBoardSelect(gameBoardSelect) {
-  if (bluePlayerNestArr[gameBoardSelect - 1].includes('finished') === true){
+  if (bluePlayerNestArr[gameBoardSelect - 1].includes('finished') === true) {
     focusUnfinishedBoards()
   } else {
     boardFocus(gameBoardSelect)
@@ -163,7 +171,7 @@ function relativeBoardSelect(gameBoardSelect) {
 function hoverAnimation(event) {
   if (event.target.classList.contains('yellow') === true && event.target.classList.contains('blue') === true) {
     return
-  } else if (playersTurn === 'blue-player'){
+  } else if (playersTurn === 'blue-player') {
     event.target.classList.add('shadow-pop-tr-blue')
   } else if (playersTurn === 'yellow-player') {
     event.target.classList.add('shadow-pop-tr-yellow')
